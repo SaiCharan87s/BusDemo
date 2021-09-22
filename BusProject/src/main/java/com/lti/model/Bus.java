a9d2212a5370bb11854b4db4b718181129df6579
mithumarymanoj <mithumarymanoj@gmail.com>
2021-09-22 23:07:11 +0530

    Committed by Mithu

---
+++ BusProject/src/main/java/com/lti/model/Bus.java
@@ -0,0 +1,63 @@
+package com.lti.model;
+
+import java.time.LocalTime;
+
+public class Bus {
+	int busId;
+	String source;
+	String destination;
+	int numberOfSeats;
+	double fare;
+	String travelAgency;
+	LocalTime startTime;
+	LocalTime endTime;
+	public int getBusId() {
+		return busId;
+	}
+	public void setBusId(int busId) {
+		this.busId = busId;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public int getNumberOfSeats() {
+		return numberOfSeats;
+	}
+	public void setNumberOfSeats(int numberOfSeats) {
+		this.numberOfSeats = numberOfSeats;
+	}
+	public double getFare() {
+		return fare;
+	}
+	public void setFare(double fare) {
+		this.fare = fare;
+	}
+	public String getTravelAgency() {
+		return travelAgency;
+	}
+	public void setTravelAgency(String travelAgency) {
+		this.travelAgency = travelAgency;
+	}
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+	
+}
--- BusProject/src/main/java/com/lti/model/UserDetails.java
+++ BusProject/src/main/java/com/lti/model/UserDetails.java
@@ -1,5 +1,7 @@
 package com.lti.model;
 
 public class UserDetails {
-
+public UserDetails() {
+	// TODO Auto-generated constructor stub
+}
 }

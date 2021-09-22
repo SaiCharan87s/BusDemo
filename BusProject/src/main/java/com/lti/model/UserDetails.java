6aac846c58d2ee607d870c47dcfad05563b7678a
SaiCharan87s <pickraju24@gmail.com>
2021-09-22 23:01:24 +0530

    UserName Commit Sai

--- BusProject/src/main/java/com/lti/model/UserDetails.java
+++ BusProject/src/main/java/com/lti/model/UserDetails.java
@@ -1,5 +1,84 @@
 package com.lti.model;
 
 public class UserDetails {
+	int userId;
+	String userName;
+	String password;
+	String emailId;
+	String firstName;
+	String lastName;
+	String phoneNumber;
+
+	public UserDetails() {
+
+	}
+
+	public UserDetails(int userId, String userName, String password, String emailId, String firstName, String lastName,
+			String phoneNumber) {
+
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.emailId = emailId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
 }

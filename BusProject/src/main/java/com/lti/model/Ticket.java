cbc31e75887147020ae6c88b719268b053e0e3e8
divyagnanaguru <divyagnanaguru99@gmail.com>
2021-09-22 23:07:14 +0530

    Added Admin, Guest User and Ticket

---
+++ BusProject/src/main/java/com/lti/model/Ticket.java
@@ -0,0 +1,34 @@
+package com.lti.model;
+
+public class Ticket {
+	
+	int ticketId;
+	int seatNumber;
+	
+	public Ticket() {
+		
+	}
+
+	public Ticket(int ticketId, int seatNumber) {
+		super();
+		this.ticketId = ticketId;
+		this.seatNumber = seatNumber;
+	}
+
+	public int getTicketId() {
+		return ticketId;
+	}
+
+	public void setTicketId(int ticketId) {
+		this.ticketId = ticketId;
+	}
+
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+
+	public void setSeatNumber(int seatNumber) {
+		this.seatNumber = seatNumber;
+	}
+
+}

4bf560ae3d203cfcce3ebf7728abdec37e1d8110
Acer <Acer@Svr>
2021-09-23 21:03:00 +0530

    booking

---
+++ BusProject/src/main/java/com/lti/Dao/BookingDao.java
@@ -0,0 +1,10 @@
+package com.lti.Dao;
+
+//import java.util.List;
+
+public interface BookingDao {
+//	public addBookingwithExistingBus();
+//	public List<E> viewAvailableSeats();
+//	public List<E> viewNoOfSeatsBooked();
+
+}

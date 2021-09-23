2fc1b387507fed03a8df647301813030b57227f6
unknown <saravananraaj255@gmail.com>
2021-09-23 22:26:53 +0530

    First Commit

---
+++ BusProject/src/main/java/com/lti/Dao/BookingDaoImpl.java
@@ -0,0 +1,19 @@
+package com.lti.Dao;
+
+import com.lti.model.Bus;
+
+public class BookingDaoImpl implements BookingDao {
+
+	public void addBookingwithExistingBus(Bus bus)
+	{
+		
+	}
+	public int viewAvailableSeats(int busId)
+	{
+		return 0;
+	}
+	public int viewNoOfSeatsBooked(int busId)
+	{
+		return 0;
+	}
+}

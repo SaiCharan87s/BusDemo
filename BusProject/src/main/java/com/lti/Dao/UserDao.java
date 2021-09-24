0a7dd95890e467e143a81f6d7676acbba5a14f96
SaiCharan87s <pickraju24@gmail.com>
2021-09-24 14:24:05 +0530

    user commit

--- BusProject/src/main/java/com/lti/Dao/UserDao.java
+++ BusProject/src/main/java/com/lti/Dao/UserDao.java
@@ -1,5 +1,13 @@
 package com.lti.Dao;
 
+import java.util.List;
+
+import com.lti.model.UserDetails;
+
 public interface UserDao {
 
+	UserDetails addOrUpdateUser(UserDetails user);
+	UserDetails findUserById(int userId);
+	List<UserDetails> viewUserDetails();
+	boolean loginUser(String email, String password);
 }
--- BusProject/src/main/java/com/lti/Dao/UserDaoImpl.java
+++ BusProject/src/main/java/com/lti/Dao/UserDaoImpl.java
@@ -1,5 +1,33 @@
 package com.lti.Dao;
 
+import java.util.List;
+
+import com.lti.model.UserDetails;
+
 public class UserDaoImpl implements UserDao {
+
+	@Override
+	public UserDetails addOrUpdateUser(UserDetails user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDetails findUserById(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserDetails> viewUserDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean loginUser(String email, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
